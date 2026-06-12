c24df0a1f74b39d1b464b44ffc623bdba40c9e9b
ermis89 <andreadakisgiannis@gmail.com>
2026-06-12 16:38:49 +0300

    Add initial teams dataset

---
+++ src/data/teams.js
@@ -0,0 +1,14 @@
+export const teams = [
+  { code: 'ARG', name: 'Argentina', confederation: 'CONMEBOL', group: 'J', flag: '🇦🇷', fifaRank: 1, titles: 3 },
+  { code: 'BRA', name: 'Brazil', confederation: 'CONMEBOL', group: 'C', flag: '🇧🇷', fifaRank: 5, titles: 5 },
+  { code: 'FRA', name: 'France', confederation: 'UEFA', group: 'I', flag: '🇫🇷', fifaRank: 2, titles: 2 },
+  { code: 'ESP', name: 'Spain', confederation: 'UEFA', group: 'H', flag: '🇪🇸', fifaRank: 3, titles: 1 },
+  { code: 'ENG', name: 'England', confederation: 'UEFA', group: 'L', flag: '🏴', fifaRank: 4, titles: 1 },
+  { code: 'GER', name: 'Germany', confederation: 'UEFA', group: 'E', flag: '🇩🇪', fifaRank: 10, titles: 4 },
+  { code: 'USA', name: 'United States', confederation: 'CONCACAF', group: 'D', flag: '🇺🇸', fifaRank: 16, titles: 0 },
+  { code: 'MEX', name: 'Mexico', confederation: 'CONCACAF', group: 'A', flag: '🇲🇽', fifaRank: 15, titles: 0 },
+  { code: 'CAN', name: 'Canada', confederation: 'CONCACAF', group: 'B', flag: '🇨🇦', fifaRank: 31, titles: 0 },
+  { code: 'POR', name: 'Portugal', confederation: 'UEFA', group: 'K', flag: '🇵🇹', fifaRank: 6, titles: 0 },
+  { code: 'NED', name: 'Netherlands', confederation: 'UEFA', group: 'F', flag: '🇳🇱', fifaRank: 7, titles: 0 },
+  { code: 'BEL', name: 'Belgium', confederation: 'UEFA', group: 'G', flag: '🇧🇪', fifaRank: 8, titles: 0 },
+];
