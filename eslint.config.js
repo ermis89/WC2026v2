3e6f7025aa0f9f5f732f96286319d9a29d2eface
ermis89 <andreadakisgiannis@gmail.com>
2026-06-12 18:16:03 +0300

    Disable React purity lint rule for countdown timer

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/purity': 'off',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
